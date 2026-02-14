ebccabf1eb9780f8bf47704b6e1b223cb091f5c8
nicolasdumitru <nicolas@ndumitru.com>
2026-02-15 00:54:24 +0200

    chore: Update boilerplate

--- src/main.cpp
+++ src/main.cpp
@@ -8,21 +8,18 @@
 // Submission URL:
 // Verdict:
 
-#include <cstddef>
 #include <cstdint>
 #include <iostream>
+#include <istream>
 #include <vector>
 
-using usize = std::size_t;
-using isize = std::ptrdiff_t;
-using u8 = std::uint8_t; // std input streams treat this as char
-using i8 = std::int8_t;  // std input streams treat this as char
-using u16 = std::uint16_t;
-using i16 = std::int16_t;
-using u32 = std::uint32_t;
+using namespace std;
+
 using i32 = std::int32_t;
-using u64 = std::uint64_t;
 using i64 = std::int64_t;
+
+// using u32 = std::uint32_t;
+// using u64 = std::uint64_t;
 
 template <typename T>
 inline auto read(std::istream &input = std::cin) -> T {
@@ -32,7 +29,7 @@ inline auto read(std::istream &input = std::cin) -> T {
 }
 
 template <typename T>
-inline auto read_vector(usize n, std::istream &input = std::cin) -> std::vector<T> {
+inline auto read_vector(i64 n, std::istream &input = std::cin) -> std::vector<T> {
     std::vector<T> v(n);
     for (auto &&x : v) {
         x = read<T>(input);
