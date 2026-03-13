c3758266c5aa7593bbc6314b91fbf818be3a464d
nicolasdumitru <nicolas@ndumitru.com>
2026-03-14 00:38:00 +0200

    CSES: Update "Edit Distance" solution

--- solutions/cses/edit-distance/iterative.cpp
+++ solutions/cses/edit-distance/iterative.cpp
@@ -5,7 +5,7 @@
 // === Solution Information ===
 // Copyright (C) 2025 Nicolas Dumitru
 // SPDX-License-Identifier: GPL-3.0-or-later
-// Submission URL: https://cses.fi/problemset/result/16477398/
+// Submission URL: https://cses.fi/problemset/result/16548698/
 // Verdict: ACCEPTED
 
 #include <algorithm>
@@ -20,18 +20,20 @@
 using i32 = std::int32_t;
 
 auto levenshtein_distance(const std::vector<char> &a, const std::vector<char> &b) -> i32 {
-    auto prev_row = std::vector<i32>(std::ssize(b) + 1);
-    auto curr_row = std::vector<i32>(std::ssize(b) + 1);
+    const auto [shorter, longer] = (a.size() < b.size()) ? std::tie(a, b) : std::tie(b, a);
 
-    for (auto j = i32{0}; j <= std::ssize(b); j += 1) {
+    auto prev_row = std::vector<i32>(std::ssize(shorter) + 1);
+    auto curr_row = std::vector<i32>(std::ssize(shorter) + 1);
+
+    for (auto j = i32{0}; j <= std::ssize(shorter); j += 1) {
         curr_row[j] = j;
     }
 
-    for (auto i = i32{1}; i <= std::ssize(a); i += 1) {
+    for (auto i = i32{1}; i <= std::ssize(longer); i += 1) {
         std::swap(curr_row, prev_row);
         curr_row[0] = i;
-        for (auto j = i32{1}; j <= std::ssize(b); j += 1) {
-            curr_row[j] = a[i - 1] == b[j - 1]
+        for (auto j = i32{1}; j <= std::ssize(shorter); j += 1) {
+            curr_row[j] = longer[i - 1] == shorter[j - 1]
                               ? prev_row[j - 1]
                               : 1 + std::min({curr_row[j - 1], prev_row[j - 1], prev_row[j]});
         }
